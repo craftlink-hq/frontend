7aae3813112bceadba64ec0ec721100b4315a06e
pope-h <hamatthan@gmail.com>
2025-07-09 23:03:01 +0100

    feat: added logic for loading state for artisan manage-jobs

--- app/manage-jobs/artisans/completed/page.tsx
+++ app/manage-jobs/artisans/completed/page.tsx
@@ -3,26 +3,38 @@ import { appliedJobFilters } from "@/utils/filters";
 import CompletedJob from "@/components/ManageJobs/Completed";
 import ManageJobs from "@/components/ManageJobs/Job";
 import { useFetchArtisanCompletedGigs } from "@/hooks/ManageJob/ArtisanHooks/useFetchArtisanCompletedGigs";
+import Loading from "@/components/Loading";
 
 export default function CompletedJobs() {
-  const { completedGigs: Completed } = useFetchArtisanCompletedGigs();
+  const { completedGigs: Completed, isLoading, error } = useFetchArtisanCompletedGigs();
+
+  if (error) {
+    console.error("Error fetching completed gigs:", error);
+    return (
+      <div className="text-red-500">
+        Error fetching completed jobs. Please try again later.
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <ManageJobs
-        title={"You have no completed jobs at the moment"}
-        desc={
-          "Complete your first job to see it listed here and start building your reputation."
-        }
-        imageSrc={"/completed.png"}
-        filters={appliedJobFilters}
-        jobs={Completed}
-        JobStatus={CompletedJob}
-        jobType={"completed"}
-        pageDetails={
-          "Celebrate your handwork! View the jobs you’ve successfully completed and the feedback received from clients."
-        }
-      />
-    </div>
+    <Loading show={isLoading}>
+      <div>
+        <ManageJobs
+          title={"You have no completed jobs at the moment"}
+          desc={
+            "Complete your first job to see it listed here and start building your reputation."
+          }
+          imageSrc={"/completed.png"}
+          filters={appliedJobFilters}
+          jobs={Completed}
+          JobStatus={CompletedJob}
+          jobType={"completed"}
+          pageDetails={
+            "Celebrate your handwork! View the jobs you’ve successfully completed and the feedback received from clients."
+          }
+        />
+      </div>
+    </Loading>
   );
 }
