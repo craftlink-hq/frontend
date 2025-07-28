2b5ddb5b6cbf171696e80dfb45aa08c4cea954c3
pope-h <hamatthan@gmail.com>
2025-07-29 00:41:53 +0100

    feat: logic added for getting artisan client

--- hooks/ReviewSystem/useGetArtisanReviews.ts
+++ hooks/ReviewSystem/useGetArtisanReviews.ts
@@ -1,29 +1,31 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { getReviewContract } from "@/constants/contracts";
+import { useEffect, useState, useCallback } from "react";
 import { readOnlyProvider } from "@/constants/providers";
 import { toast } from "sonner";
 
-export const useGetArtisanReviews = (artisanAddress: string) => {
+const useGetArtisanReviews = (artisanAddress: string) => {
   const [comments, setComments] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const contract = getReviewContract(readOnlyProvider);
-        const commentHashes = await contract.getArtisanReviews(artisanAddress);
-        setComments(commentHashes);
-      } catch (error) {
-        toast.error("Error fetching artisan review comments");
-        console.error("Error fetching artisan review comments:", error);
-      }
-    };
-
-    if (artisanAddress) {
-      fetchComments();
+  const fetchComments = useCallback(async () => {
+    try {
+      const contract = getReviewContract(readOnlyProvider);
+      const commentHashes = await contract.getArtisanReviews(artisanAddress);
+      setComments(commentHashes);
+    } catch (error) {
+      toast.error("Error fetching artisan review comments");
+      console.error("Error fetching artisan review comments:", error);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [artisanAddress]);
+
+  useEffect(() => {
+    fetchComments();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return comments;
 };
+
+export default useGetArtisanReviews;
