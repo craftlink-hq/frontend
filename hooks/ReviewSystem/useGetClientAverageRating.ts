837508b96fd74cac648af1c8961e5e6f1e8ec87f
pope-h <hamatthan@gmail.com>
2025-07-29 13:42:39 +0100

    feat: logic added for getting client average rating

--- hooks/ReviewSystem/useGetClientAverageRating.ts
+++ hooks/ReviewSystem/useGetClientAverageRating.ts
@@ -1,33 +1,29 @@
 "use client";
 
 import { getReviewContract } from "@/constants/contracts";
-import { useAccount } from "@/lib/thirdweb-hooks";
 import { useEffect, useState, useCallback } from "react";
 import { readOnlyProvider } from "@/constants/providers";
 import { toast } from "sonner";
 
-const useGetClientAverageRating = () => {
-  const { address, isConnected } = useAccount();
+const useGetClientAverageRating = (clientAddress: string) => {
   const [clientRating, setClientRating] = useState<number | null>(null);
 
   const fetchClientRating = useCallback(async () => {
-    if (!address) return;
-
     try {
       const contract = getReviewContract(readOnlyProvider);
-      const rating = await contract.getClientAverageRating(address);
+      const rating = await contract.getClientAverageRating(clientAddress);
       setClientRating(rating);
     } catch (error) {
       toast.error("Error checking client average rating");
       console.error("Error checking client rating:", error);
       setClientRating(null);
     }
-  }, [address]);
+  }, [clientAddress]);
 
   useEffect(() => {
     fetchClientRating();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isConnected]);
+  }, []);
 
   return clientRating;
 };
